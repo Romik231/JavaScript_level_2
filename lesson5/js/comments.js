37aa8b069e747ae0881f6e9428ef827583252675
Romik231 <Romik231@yandex.ru>
2018-12-20 15:09:14 +0300

    added hw5

---
+++ lesson5/js/comments.js
@@ -0,0 +1,24 @@
+$(document).ready(() => {
+
+    
+    
+    
+    function getComment() {
+        
+    }
+
+    function addComment() {
+
+    }
+
+    function getTextComment() {
+
+    }
+
+    function addTextComment() {
+
+    }
+
+});
+//Получение количества комментариев
+
